78ff1d5dd9c191366374d0a52b348807597530f1
Yongseung Song <yongseung.song@icloud.com>
2023-11-29 20:09:34 +0900

    refactoring: 구조 재정비

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,21 +1,17 @@
 import Layout from 'components/layout/Layout';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import store from 'redux/config/configStore';
 import Detail from '../pages/Detail';
 import Home from '../pages/Home';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="details/:id" element={<Detail />} />
-          </Route>
-        </Routes>
-      </Provider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="details/:id" element={<Detail />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
